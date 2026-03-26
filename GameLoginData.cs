68ca3b45fab64072708202d2f2fdd45093d28723
Mefodei <mefodei.link@gmail.com>
2026-03-26 22:55:59 +0700

    - add odin support

--- GameLoginData.cs
+++ GameLoginData.cs
@@ -4,6 +4,14 @@ namespace UniGame.Runtime.GameAuth
     using UnityEngine;
     using UnityEngine.AddressableAssets;
     using UnityEngine.Localization;
+    
+#if  UNITY_EDITOR
+    using UniModules.Editor;
+#endif
+
+#if ODIN_INSPECTOR
+    using Sirenix.OdinInspector;
+#endif
 
     [Serializable]
     public class GameLoginData
@@ -14,9 +22,23 @@ namespace UniGame.Runtime.GameAuth
         public AssetReferenceSprite icon;
         public LocalizedString title;
         public LocalizedString description;
-        
+
+#if ODIN_INSPECTOR
+        [InlineButton(nameof(OpenScript), "Open Script")]
+#endif
         [SerializeReference]
         public IAuthProviderFactory authFactory;
+
+        
+        public void OpenScript()
+        {
+#if  UNITY_EDITOR
+            if (authFactory == null) return;
+            var type = authFactory.GetType();
+            type.OpenScript();
+#endif
+        }
+
     }
 
     public enum AuthType
